38dd77abfdd907467f25f605c7fcfe717e15b39a
DWils <45233723+DWils@users.noreply.github.com>
2021-01-25 10:23:17 +0100

    listeChambres devient dataChambres
    Pour eviter une confusion avec la listeChambres dans Hotel , j'ai modifié le nom de la listeChambres dans main en dataChambres

--- GestionHotel/src/fr/afpa/main/Main.java
+++ GestionHotel/src/fr/afpa/main/Main.java
@@ -40,7 +40,7 @@ public class Main {
 			}
 		}
 
-		String[] listeChambres = { "Type chambre;Taille;Vues;Occupation;tarif;Nombre;Options",
+		String[] dataChambres = { "Type chambre;Taille;Vues;Occupation;tarif;Nombre;Options",
 				"Chambre Vue Piscine;44 mètres carrés;Piscine Centrale;2 adultes et 2 enfants de moins de 12 ans;300;7;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
 				"Chambre Vue Jardin;44 mètres carrés;Jardin, Forêt ou Golf;2 adultes et 2 enfants de moins de 12 ans;314;12;Fer et planche à repasser sur demande|Téléphone |Télévision par câble|Climatisation|Bouilloire électrique|Concierge 24h/24",
 				"Chambre Vue Océan;44 mètres carrés;Partielle Océan et Forêt ou Golf;2 adultes et 2 enfants de moins de 12 ans;350;8;Fer et planche à repasser sur demande|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
@@ -50,9 +50,9 @@ public class Main {
 				"Suite Ambassadeur;230 mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;1650;7;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24",
 				"Suite Royale;342  mètres carrés;Océan;2 adultes et 2 enfants de moins de 12 ans;2400;4;Sèche-cheveux|Coffre-fort dans la chambre|Minibar|Téléphone |Accès Internet haut débit sans fil|Lecteur DVD sur demande|Télévision par câble|Climatisation|Service aux chambres 24h/24|Concierge 24h/24" };
 
-		for (int i2 = 1; i2 < listeChambres.length; i2++) {
+		for (int i2 = 1; i2 < dataChambres.length; i2++) {
 
-			infosChambre = listeChambres[i2].split(";");
+			infosChambre = dataChambres[i2].split(";");
 
 			nbChambre = nbChambre + Integer.parseInt(infosChambre[5]);
 			
@@ -60,8 +60,8 @@ public class Main {
 		System.out.println(nbChambre); //Check up nombre total de chambre
 		Chambre[] enregistrementChambre = new Chambre[nbChambre];
 		chambre = new Chambre();
-		for (int i = 1; i < listeChambres.length; i++) {
-			infosChambre = listeChambres[i].split(";");
+		for (int i = 1; i < dataChambres.length; i++) {
+			infosChambre = dataChambres[i].split(";");
 
 			for (int j = 0; j < Integer.parseInt(infosChambre[5]) ; j++) {
 				numeroChambre++;
