4192a449943b4ceed62400094be643762e635891
DWils <45233723+DWils@users.noreply.github.com>
2021-01-25 14:32:59 +0100

    Update Client.java

--- GestionHotel/src/fr/afpa/main/Client.java
+++ GestionHotel/src/fr/afpa/main/Client.java
@@ -4,5 +4,18 @@ public class Client {
 	private String nom;
 	private String prenom;
 	private String telephone;
-	private String listReservation;
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }
