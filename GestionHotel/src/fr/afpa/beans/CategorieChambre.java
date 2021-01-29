78f72c21db7bedf4ec5a2fca9621439e8ebba255
David Wils <david.wils591@gmail.com>
2021-01-29 12:06:34 +0100

    maj complete des affichages reservation chambre et CategorieChambre

---
+++ GestionHotel/src/fr/afpa/beans/CategorieChambre.java
@@ -0,0 +1,123 @@
+package fr.afpa.beans;
+
+import fr.afpa.services.GestionChambre;
+
+public class CategorieChambre {
+	private static int nbCategorie;
+	private int id;
+	private String nom;
+	private String superficie;
+	private String vue;
+	private float tarif;
+	private String[] options;
+	private String occupation;
+	private Chambre[] listeChambres;
+
+	public CategorieChambre(String unNom, 
+			String uneSuperficie, String uneVue, 
+			float unTarif, String[] desOptions,
+			String uneOccupation, Chambre[] listeCh) {
+		id = nbCategorie;
+		nom = unNom;
+		superficie = uneSuperficie;
+		vue = uneVue;
+		tarif = unTarif;
+		options = desOptions;
+		occupation = uneOccupation;
+		listeChambres = listeCh;
+		nbCategorie++;
+	}
+
+	public CategorieChambre() {
+		nom = new String("");
+		superficie = new String("");
+		vue = new String("");
+		tarif = 0;
+		options = new String[10];
+		listeChambres = new Chambre[15];
+	}
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+	public void setOccupation(String uneOccupation) {
+		this.occupation = uneOccupation;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String unNom) {
+		nom = unNom;
+	}
+
+	public String getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(String uneSuperficie) {
+		superficie = uneSuperficie;
+	}
+
+	public String getVue() {
+		return vue;
+	}
+
+	public void setVue(String uneVue) {
+		vue = uneVue;
+	}
+
+	public float getTarif() {
+		return tarif;
+	}
+
+	public void setTarif(float unTarif) {
+		tarif = unTarif;
+	}
+
+	public String[] getOptions() {
+		return options;
+	}
+
+	public void setOptions(String[] desOptions) {
+		options = desOptions;
+	}
+
+	public Chambre[] getListeChambres() {
+		return listeChambres;
+	}
+
+	public void setListeChambres(Chambre[] listeCh) {
+		listeChambres = listeCh;
+	}
+
+	public void afficheCategorieChambre() {
+
+		System.out.println("----------------------------------------------");
+		System.out.println("Type n° " + id + " " + nom + " " + tarif + " euros \n" + occupation + "\n "
+				+ "Superficie : " + superficie + "\n " + "Vue : " + vue + "\n " + "Liste options :");
+		afficheOptions();
+
+	}
+
+	public void afficheOptions() {
+		for (int i = 0; i < options.length; i++) {
+			System.out.println("   " + options[i]);
+
+		}
+	}
+
+	public void afficheCategorieChambreAvecChambre() {
+
+		afficheCategorieChambre();
+		for (int i = 0; i < listeChambres.length; i++) {
+			if(listeChambres[i] != null) {
+				listeChambres[i].afficherChambre();
+			}
+		}
+
+	}
+
+}
--- GestionHotel/src/fr/afpa/beans/Chambre.java
+++ GestionHotel/src/fr/afpa/beans/Chambre.java
@@ -1,98 +1,32 @@
 package fr.afpa.beans;
 
 public class Chambre {
-	private static int reservee;
-	private static int ocuppee;
+	private static int nbChambre;
+	private static int nbReservee;
+	private static int nbOcuppee;
 	private int numero;
-	private String nom;
-	private String superficie;
-	private String vue;
-	private float tarif;
-	private String[] options;
-	private String occupation;
 	private Reservation[] listeReservation;
-	
 	private String etat;
-	private int nbChambre;
-	public static Chambre[] hotel;
 
-	public Chambre(int num , String n ,String sup, String v ,float t , String[] opts, Reservation[] listeR ) {
-		numero = num;
-		setNbChambre(1);
-		nom = n;
-		superficie = sup;
-		vue = v;
-		tarif = t;
-		options = opts;
+	public Chambre(int num , Reservation[] listeR, String unEtat ) {
+		numero = nbChambre;
 		listeReservation = listeR;
-		
+		etat = unEtat;	
 	}
 	
 	public Chambre() {
 		numero = 0;
 		setNbChambre(1);
-		nom = new String("");
-		superficie = new String("");;
-		vue = new String("");;
-		tarif = 0;
-		options = new String[10];
 		listeReservation = new Reservation[3];
+		etat = "libre";
 	}
 
 	public int getNumero() {
 		return numero;
 	}
 	
-	public String getNom() {
-		return nom;
-	}
-	
-	public String getSuperficie() {
-		return superficie;
-	}
-
-	public void setSuperficie(String sup) {
-		superficie = sup;
-	}
-
-	public double getTarif() {
-		return tarif;
-	}
-	
-	public String[] getOptions () {
-		return options;
-	}
-	
 	public void setNumero(int num) {
 		numero = num;
-	}
-	
-	public void setNom(String n) {
-		nom = n;
-	}
-	
-	public void setTarif(float tf) {
-		tarif = tf;
-	}
-	
-	public void setOptions(String[] opts) {
-		options = opts;
-	}
-	
-	public String getOccupation() {
-		return occupation;
-	}
-
-	public void setOccupation(String oc) {
-		occupation = oc;
-	}
-
-	public String getVue() {
-		return vue;
-	}
-
-	public void setVue(String v) {
-		vue = v;
 	}
 
 	public Reservation[] getListeReservation() {
@@ -103,27 +37,23 @@ public class Chambre {
 		listeReservation = listeR;
 	}
 
-	public static int getReservee() {
-		return reservee;
-	}
-
-	public static void setReservee(int reservee) {
-		Chambre.reservee = reservee;
-	}
-
-	public static int getOcuppee() {
-		return ocuppee;
-	}
-
-	public static void setOcuppee(int oc) {
-		ocuppee = oc;
-	}
-
 	public int getNbChambre() {
 		return nbChambre;
 	}
 
 	public void setNbChambre(int nbc) {
 		nbChambre = nbc;
+	}
+
+	public String getEtat() {
+		return etat;
+	}
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+	
+	public void afficherChambre() {
+		System.out.println( );
 	}
 }
